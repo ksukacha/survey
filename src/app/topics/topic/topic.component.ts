b5f12f05c969fc2c74eb1963284be4e9c856ab97
ksukacha <kachytskaya@gmail.com>
2019-11-04 03:00:53 +0300

    topic.component: added checkboxes, able to identify which question was clicked

--- src/app/topics/topic/topic.component.ts
+++ src/app/topics/topic/topic.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {TopicModel} from '../../model/topic.model';
 import {TopicsService} from '../topics.service';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-topic',
@@ -10,6 +11,7 @@ import {TopicsService} from '../topics.service';
 })
 export class TopicComponent implements OnInit {
   topic: TopicModel;
+  selectedIndexes: Array<boolean> = [];
   constructor(private activatedRoute: ActivatedRoute,
               private topicsService: TopicsService) { }
 
@@ -17,6 +19,16 @@ export class TopicComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(params => {
       this.topic = this.topicsService.getTopic(params.get('id'));
     });
+    for (const i of this.topic.questions) {
+      this.selectedIndexes.push(false);
+    }
+    console.log(this.selectedIndexes[0]);
   }
 
+  addToTopic(): void {
+    console.log(this.selectedIndexes.length);
+    for (let i = 0; i < this.selectedIndexes.length; ++i) {
+      console.log(i, this.selectedIndexes[i]);
+    }
+  }
 }
